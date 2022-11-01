060259e99fa73139619dd54d344d95e29b2fc1dc
63csmith <63csmith@gmail.com>
2022-11-01 16:42:40 -0600

    test auth

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { withAuthenticator } from "@aws-amplify/ui-react";
+import { NavBar } from "./components/NavBar";
 
 function App({ signOut, user }) {
   return (
     <div className="App">
+      <NavBar />
       {user.attributes.email}
       <button onClick={signOut}>Sign Out</button>
     </div>
---
+++ src/components/NavBar.js
@@ -0,0 +1,26 @@
+import { useState, useEffect } from "react";
+import { Navbar, Nav, Container } from "react-bootstrap";
+export const NavBar = () => {
+  const [activeLink, setActiveLink] = useState("home");
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const onScroll = () => {
+      if (window.scrollY > 50) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", onScroll);
+
+    return () => window.removeEventListener("scroll", onScroll);
+  }, []);
+
+  const onUpdateActiveLink = (value) => {
+    setActiveLink(value);
+  };
+
+  return <Navbar></Navbar>;
+};
